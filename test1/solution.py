146d2105a8ac5d8a9354ade61d0bf46244d62c0a
PikaAkip123 <ole.machmerth@norbert-gymnasium.de>
2025-07-31 10:11:34 +0000

    solution.py

--- test1/solution.py
+++ test1/solution.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+print("Hallo, Welt!")
+
+
+# print (name)
+
+def print_name(name):
+    if name == 'Ole' or name == 'ole' :
+        print('Schön dich zu sehen',name)
+    else:
+        print('Hallo', name, 'Du bist neu hier')
+
+while True:
+    vorname = input ('Wie ist dein Name?')                      # Wartet auf Benutzereingabe
+    if vorname == 'q' or vorname == 'Q' :
+        break
+    print_name(vorname)
